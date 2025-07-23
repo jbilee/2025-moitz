13871aee678e5971e78fddbb7388bd030ed2663e
julie <7jujubuilds@gmail.com>
2025-07-23 21:55:11 +0900

    test

---
+++ backend/src/main/java/com/f12/moitz/controller/JulieController.java
@@ -0,0 +1,18 @@
+package com.f12.moitz.controller;
+
+import com.f12.moitz.domain.Gumball;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/julie")
+public class JulieController {
+
+    @GetMapping
+    public ResponseEntity<Gumball> get() {
+        return ResponseEntity.ok(new Gumball("Tuda", 20));
+    }
+
+}
---
+++ backend/src/main/java/com/f12/moitz/domain/Gumball.java
@@ -0,0 +1,7 @@
+package com.f12.moitz.domain;
+
+public record Gumball(
+        String name,
+        int age
+) {
+}
